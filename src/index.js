2be957850bc1c1b50e1ed265a943aaf17e4a7ced
Abdul Qadeer Solangi <abdulqadeersolangi007@gmail.com>
2021-01-29 01:50:31 +0500

    state in react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+function Room() {
+  const [value, setValue] = React.useState(true);
+  
+   function Click () {
+   setValue((prevValue) => !prevValue)
+   }
+
+  return (
+    <div>
+      <div>The room is{(value === true) ? "Bright" : "Dark"}    </div>
+     
+      <button onClick={Click}> Change </button>
+    </div>
+  );
+}
+
+ReactDOM.render(<Room />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
